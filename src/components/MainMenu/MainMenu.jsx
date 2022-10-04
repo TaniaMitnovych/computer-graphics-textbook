500d4b7760b9cf73e3512b40c0c30354b85ef6c7
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-10-04 23:45:40 +0300

    changed styles of main menu component

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -2,27 +2,38 @@ import React from 'react';
 import './MainMenu.css';
 const MainMenu = () => {
 
-  function handleHelpButtonClick(){
+  function hideMainMenu(){
     let mainMenu=document.querySelector(".main-menu");
-    console.log(mainMenu)
     mainMenu.style.display="none";
+  }
+  function handleHelpButtonClick(){
+    hideMainMenu();
+  }
+  function handleClick(e){
+    let target='#'+e.currentTarget.value;
+    console.log(target);
+    let sectionItem=document.querySelector(target);
+    console.log(sectionItem);
+    let event = new Event("click");
+    sectionItem.dispatchEvent(event);
+    hideMainMenu();
   }
   return (
     <div className='main-menu'>
       <div className='main-menu__left-side'>
         <h1 className='main-menu__logo'>M</h1>
-        <button className='main-menu__button main-menu__help-button button' type='button' onClick={handleHelpButtonClick}>Help</button>
+        <button className='main-menu__button main-menu__help-button button' value='help-section' type='button' onClick={(e)=>handleClick(e)}>Help</button>
       </div>
       <div className='main-menu__right-side'>
         <ul className='main-menu__section-list'>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button'>Fractals</button>
+            <button className='main-menu__button main-menu__section-button button' value='fractal-section' type='button' onClick={(e)=>handleClick(e)}>Fractals</button>
           </li>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button'>Color schemes</button>
+            <button className='main-menu__button main-menu__section-button button' value='colors-section' type='button' onClick={(e)=>handleClick(e)}>Color schemes</button>
           </li>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button'>Object movement</button>
+            <button className='main-menu__button main-menu__section-button button' value='movement-section' type='button' onClick={(e)=>handleClick(e)}>Object movement</button>
           </li>
         </ul>
       </div>

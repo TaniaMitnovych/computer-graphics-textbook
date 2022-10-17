e888454c9e5e17f11df71c7a7da799cfdd7cf1fc
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-10-17 23:47:17 +0300

    modified function for handling clicks on menu

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -10,7 +10,8 @@ const MainMenu = () => {
     hideMainMenu();
   }
   function handleClick(e){
-    let target='#'+e.currentTarget.value;
+    console.log(e);
+    let target='#'+e.target.dataset['value'];
     console.log(target);
     let sectionItem=document.querySelector(target);
     console.log(sectionItem);
@@ -22,18 +23,18 @@ const MainMenu = () => {
     <div className='main-menu'>
       <div className='main-menu__left-side'>
         <h1 className='main-menu__logo'>M</h1>
-        <button className='main-menu__button main-menu__help-button button' value='help-section' type='button' onClick={(e)=>handleClick(e)}>Help</button>
+        <button className='main-menu__button main-menu__help-button button' data-value='help-section' type='button' onClick={(e)=>handleClick(e)}>Help</button>
       </div>
       <div className='main-menu__right-side'>
         <ul className='main-menu__section-list'>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button' value='fractal-section' type='button' onClick={(e)=>handleClick(e)}>Fractals</button>
+            <button className='main-menu__button main-menu__section-button button' data-value='fractal-section' type='button' onClick={(e)=>handleClick(e)}>Fractals</button>
           </li>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button' value='colors-section' type='button' onClick={(e)=>handleClick(e)}>Color schemes</button>
+            <button className='main-menu__button main-menu__section-button button' data-value='colors-section' type='button' onClick={(e)=>handleClick(e)}>Color schemes</button>
           </li>
           <li className='main-menu__section-item'>
-            <button className='main-menu__button main-menu__section-button button' value='movement-section' type='button' onClick={(e)=>handleClick(e)}>Object movement</button>
+            <button className='main-menu__button main-menu__section-button button' data-value='movement-section' type='button' onClick={(e)=>handleClick(e)}>Object movement</button>
           </li>
         </ul>
       </div>

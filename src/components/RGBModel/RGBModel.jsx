2e8c690b47a02258b129a2e2967ba710465793f1
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-10-22 00:23:39 +0300

    added function for changing rgb-values from parent component

--- src/components/RGBModel/RGBModel.jsx
+++ src/components/RGBModel/RGBModel.jsx
@@ -1,21 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './RGBModel.css';
-const RGBModel = () => {
+const RGBModel = (props) => {
   const [red, setRed]=useState(0);
   const [green,setGreen]=useState(0);
   const [blue, setBlue]=useState(0);
+  useEffect(()=>{
+    let rgbColor = document.querySelector(".rgb-model__choosen-color");
+    rgbColor.addEventListener("colorChanged",(e)=>{
+      setRed(e.target.choosenColor[0]);
+      setGreen(e.target.choosenColor[1]);
+      setBlue(e.target.choosenColor[2]);
+    })
+  },[])
   return (
     <div className='rgb-model color-model'>
       <h2>RGB</h2>
       <div className='rgb-model__values color-values'>
         <p>Red</p>
-        <input type="range" min="0" max="255" value={red} onChange={(e)=>{setRed(e.currentTarget.value)}}/>
+        <input type="range" min="0" max="255" value={red} id="red" onChange={(e)=>{setRed(e.currentTarget.value)}}/>
         <p className='rgb-model__value color-value'>{red}</p>
         <p>Green</p>
-        <input type="range" min="0" max="255" value={green} onChange={(e)=>{setGreen(e.currentTarget.value)}}/>
+        <input type="range" min="0" max="255" value={green} id="green" onChange={(e)=>{setGreen(e.currentTarget.value)}}/>
         <p className='rgb-model__value color-value'>{green}</p>
         <p>Blue</p>
-        <input type="range" min="0" max="255" value={blue} onChange={(e)=>{setBlue(e.currentTarget.value)}}/>
+        <input type="range" min="0" max="255" value={blue} id="blue" onChange={(e)=>{setBlue(e.currentTarget.value)}}/>
         <p className='rgb-model__value color-value'>{blue}</p>
       </div>
       <div className='rgb-model__choosen-color choosen-color' style={{background: `rgb(${red},${green},${blue})`}}>

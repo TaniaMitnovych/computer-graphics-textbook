c1b8e2fc47a84e72f06b0254a1bb415eef3cafe7
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-10-22 00:21:37 +0300

    added hsl color model component

---
+++ src/components/HSLModel/HSLModel.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+const HSLModel = () => {
+  const [hue, setHue]=useState(0);
+  const [saturation,setSaturation]=useState(0);
+  const [lightness, setLightness]=useState(0);
+  useEffect(()=>{
+    let hslColor = document.querySelector(".hsl-model__choosen-color");
+    hslColor.addEventListener("colorChanged",(e)=>{
+      setHue(e.target.choosenColor[0]);
+      setSaturation(e.target.choosenColor[1]);
+      setLightness(e.target.choosenColor[2]);
+    })
+  })
+  return (
+    <div className='hsl-model color-model'>
+      <h2>HSL</h2>
+      <div className='hsl-model__values color-values'>
+        <p>Hue</p>
+        <input type="range" min="0" max="360" value={hue} onChange={(e)=>{setHue(e.currentTarget.value)}}/>
+        <p className='hsl-model__value color-value'>{hue}</p>
+        <p>Saturation</p>
+        <input type="range" min="0" max="100" value={saturation} onChange={(e)=>{setSaturation(e.currentTarget.value)}}/>
+        <p className='hsl-model__value color-value'>{saturation}</p>
+        <p>Lightness</p>
+        <input type="range" min="0" max="100" value={lightness} onChange={(e)=>{setLightness(e.currentTarget.value)}}/>
+        <p className='hsl-model__value color-value'>{lightness}</p>
+      </div>
+      <div className='hsl-model__choosen-color choosen-color' style={{background: `hsl(${hue},${saturation}%,${lightness}%)`}}>
+        Color
+      </div>
+
+    </div>
+  );
+};
+
+export default HSLModel;

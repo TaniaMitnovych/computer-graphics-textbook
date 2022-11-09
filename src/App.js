f495ad8adaa7c18b2a524bfb9d7453d21d2a178d
tetiana.mitnovych.pz.2020 <tetiana.mitnovych.pz.2020@lpnu.ua>
2022-11-09 21:54:29 +0200

    deleted useless code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import MainMenu from "./components/MainMenu/MainMenu";
 import NavBar from "./components/NavBar/NavBar";
 import React, {useEffect, useState} from 'react';
 import Fractals from "./components/Fractals/Fractals";
+import ColorModels from "./components/ColorModels/ColorModels";
+import ObjMovementSection from "./components/ObjectMovement/ObjMovementSection";
 function App() {
   const [section,setSection]=useState(false);
   const sections={
-    "fractal-section": <Fractals/>
+    "fractal-section": <Fractals/>,
+    "colors-section":<ColorModels/>,
+    "movement-section":<ObjMovementSection/>
   }
   function showSection(sectionID){
     // let section=document.querySelector('#'+sectionID);
